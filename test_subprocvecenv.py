daa472c2e2df28698433526d34e3b6aa6a5b727e
Zhihan Yang <yangz2@carleton.edu>
2021-11-03 17:02:47 +0800

    minor fix

--- test_subprocvecenv.py
+++ test_subprocvecenv.py
@@ -9,95 +9,53 @@ if __name__ == '__main__':
 
     example_env = gym.make("HalfCheetah-v2")
 
-
-    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
     def make_env():
         return gym.make("HalfCheetah-v2")
 
-
     env_fns = [make_env for _ in range(16)]
+
+    def measure_performance(env):
+
+        start = time.perf_counter()
+
+        env.reset()
+        for _ in range(1000):
+            env.step_async(np.random.normal(size=(16, example_env.action_space.shape[0])))
+            observation, reward, done, information = env.step_wait()
+        env.close()
+
+        end = time.perf_counter()
+
+        return end - start
+
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
     env = ShmemVecEnv(env_fns, context='spawn')
+    print("ShmemVecEnv (context='spawn') Duration: ", measure_performance(env))
 
-    start = time.perf_counter()
-
-    env.reset()
-    for _ in range(1000):
-        env.step_async(np.random.normal(size=(16, example_env.action_space.shape[0])))
-        observation, reward, done, information = env.step_wait()
-
-    env.close()
-
-    end = time.perf_counter()
-
-    duration = end - start
-
-    print("ShmemVecEnv Duration: ", duration)
+    env = ShmemVecEnv(env_fns, context='fork')
+    print("ShmemVecEnv (context='fork') Duration: ", measure_performance(env))
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-
-    def make_env():
-        return gym.make("HalfCheetah-v2")
-
-
-    env_fns = [make_env for _ in range(16)]
     env = SubprocVecEnv(env_fns)
-
-    start = time.perf_counter()
-
-    env.reset()
-    for _ in range(1000):
-
-        env.step_async(np.random.normal(size=(16, example_env.action_space.shape[0])))
-        observation, reward, done, information = env.step_wait()
-
-    env.close()
-
-    end = time.perf_counter()
-
-    duration = end - start
-
-    print("SubprocVecEnv Duration: ", duration)
+    print("SubprocVecEnv Duration: ", measure_performance(env))
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-    env_fns = [make_env for _ in range(16)]
     env = DummyVecEnv(env_fns)
-
-    start = time.perf_counter()
-
-    env.reset()
-    for _ in range(1000):
-
-        env.step_async(np.random.normal(size=(16, example_env.action_space.shape[0])))
-        observation, reward, done, information = env.step_wait()
-
-    env.close()
-
-    end = time.perf_counter()
-
-    duration = end - start
-
-    print("DummyVecEnv Duration: ", duration)
+    print("DummyVecEnv Duration: ", measure_performance(env))
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
     env = make_env()
-
     start = time.perf_counter()
-
     env.reset()
     for _ in range(16000):
-
         obs, reward, done, info = env.step(env.action_space.sample())
-
         if done:
             env = make_env()
             env.reset()
-
     end = time.perf_counter()
-
     duration = end - start
-
     print("Standard Duration: ", duration)
