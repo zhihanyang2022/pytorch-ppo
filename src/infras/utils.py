114843ec80726c3aa7c2e82f2df320316f7c7122
Zhihan <yangz2@carleton.edu>
2022-01-12 11:38:33 +0800

    Update utils.py

--- src/infras/utils.py
+++ src/infras/utils.py
@@ -46,8 +46,8 @@ def gym_make_advanced(env_name):
 
 
 def get_device():
-    return 'cuda' if torch.cuda.is_available() else 'cpu'
-
+    # return 'cuda' if torch.cuda.is_available() else 'cpu'
+    return 'cpu'
 
 def save_net(net: nn.Module, save_dir: str, save_name: str) -> None:
     torch.save(net.state_dict(), os.path.join(save_dir, save_name))
